f99e35f2d5caa8c127ff9ddaf30bfbce46049b38
JC <jc@jccm.com.ve>
2024-12-25 09:17:25 -0300

    Anadir proxy server

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const scrapeLogic = async (res) => {
 	const browser = await puppeteer.launch({
-		args: ["--disable-setuid-sandbox", "--no-sandbox", "--single-process", "--no-zygote"],
+		args: [`--proxy-server=${process.env.SCRAPERAPI_URI}`, "--disable-setuid-sandbox", "--no-sandbox", "--single-process", "--no-zygote"],
 		executablePath: process.env.NODE_ENV === "production" ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
 	});
 	try {
