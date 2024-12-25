98348d381a150ff997e3549af9632159c0c7067a
JC <jc@jccm.com.ve>
2024-12-25 12:57:36 -0300

    Cambio de prueba a example.com

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -11,24 +11,24 @@ const scrapeLogic = async (res) => {
 		const page = await browser.newPage();
 
 		// await page.goto("https://api.scraperapi.com?api_key=3202a932c39c99daf584f5d126be1b94&url=https://patentechile.com/&device_type=desktop", { waitUntil: "networkidle0" });
-		await page.goto("https://patentechile.com/", { waitUntil: "networkidle2" });
+		await page.goto("https://example.com/", { waitUntil: "networkidle2" });
 
 		// Set screen size
 		await page.setViewport({ width: 1080, height: 1024 });
-		await page.type("#txtTerm", "svjk51", { delay: 100 });
-		await page.click("#btnConsultar");
-		await page.waitForNavigation({ waitUntil: "networkidle2" });
+		// await page.type("#txtTerm", "svjk51", { delay: 100 });
+		// await page.click("#btnConsultar");
+		// await page.waitForNavigation({ waitUntil: "networkidle2" });
 
-		// Extraer la información como texto plano
-		const rawText = await page.evaluate(() => {
-			const element = document.querySelector("#tblVehicl3-x");
-			if (element) {
-				return element.innerText;
-			}
-			return null;
-		});
+		// // Extraer la información como texto plano
+		// const rawText = await page.evaluate(() => {
+		// 	const element = document.querySelector("#tblVehicl3-x");
+		// 	if (element) {
+		// 		return element.innerText;
+		// 	}
+		// 	return null;
+		// });
 
-		res.send(rawText);
+		// res.send(rawText);
 
 		// Type into search box
 		// await page.type(".search-box__input", "automate beyond recorder");
@@ -39,13 +39,13 @@ const scrapeLogic = async (res) => {
 		// await page.click(searchResultSelector);
 
 		// Locate the full title with a unique string
-		// const textSelector = await page.waitForSelector("h1");
-		// const fullTitle = await textSelector.evaluate((el) => el.textContent);
+		const textSelector = await page.waitForSelector("h1");
+		const fullTitle = await textSelector.evaluate((el) => el.textContent);
 
 		// Print the full title
-		// const logStatement = `The title of this blog post is ${fullTitle}`;
-		// console.log(logStatement);
-		// res.send(logStatement);
+		const logStatement = `The title of this blog post is ${fullTitle}`;
+		console.log(logStatement);
+		res.send(logStatement);
 	} catch (e) {
 		console.error(e);
 		res.send(`Something went wrong while running Puppeteer: ${e}`);
