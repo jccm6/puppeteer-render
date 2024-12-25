e4aafde55d785f7a5670696a53084334bc75bae9
JC <jc@jccm.com.ve>
2024-12-25 09:10:46 -0300

    Quitar el headless

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 
 const scrapeLogic = async (res) => {
 	const browser = await puppeteer.launch({
-		headless: false,
 		args: ["--disable-setuid-sandbox", "--no-sandbox", "--single-process", "--no-zygote"],
 		executablePath: process.env.NODE_ENV === "production" ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
 	});
