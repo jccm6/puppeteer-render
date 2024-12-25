93a6881eb01313575cd5a08be6c46fbb2b423e1e
JC <jc@jccm.com.ve>
2024-12-25 09:06:47 -0300

    Cambios en el id del elemento

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -2,50 +2,41 @@ const puppeteer = require("puppeteer");
 require("dotenv").config();
 
 const scrapeLogic = async (res) => {
-  const browser = await puppeteer.launch({
-    args: [
-      "--disable-setuid-sandbox",
-      "--no-sandbox",
-      "--single-process",
-      "--no-zygote",
-    ],
-    executablePath:
-      process.env.NODE_ENV === "production"
-        ? process.env.PUPPETEER_EXECUTABLE_PATH
-        : puppeteer.executablePath(),
-  });
-  try {
-    const page = await browser.newPage();
+	const browser = await puppeteer.launch({
+		headless: false,
+		args: ["--disable-setuid-sandbox", "--no-sandbox", "--single-process", "--no-zygote"],
+		executablePath: process.env.NODE_ENV === "production" ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
+	});
+	try {
+		const page = await browser.newPage();
 
-    await page.goto("https://developer.chrome.com/");
+		await page.goto("https://developer.chrome.com/");
 
-    // Set screen size
-    await page.setViewport({ width: 1080, height: 1024 });
+		// Set screen size
+		await page.setViewport({ width: 1080, height: 1024 });
 
-    // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
+		// Type into search box
+		// await page.type(".search-box__input", "automate beyond recorder");
 
-    // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
+		// Wait and click on first result
+		// const searchResultSelector = ".search-box__link";
+		// await page.waitForSelector(searchResultSelector);
+		// await page.click(searchResultSelector);
 
-    // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
-    );
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
+		// Locate the full title with a unique string
+		const textSelector = await page.waitForSelector("#una-web-potente-span-stylecolor-000-display-blockmas-facilspan");
+		const fullTitle = await textSelector.evaluate((el) => el.textContent);
 
-    // Print the full title
-    const logStatement = `The title of this blog post is ${fullTitle}`;
-    console.log(logStatement);
-    res.send(logStatement);
-  } catch (e) {
-    console.error(e);
-    res.send(`Something went wrong while running Puppeteer: ${e}`);
-  } finally {
-    await browser.close();
-  }
+		// Print the full title
+		const logStatement = `The title of this blog post is ${fullTitle}`;
+		console.log(logStatement);
+		res.send(logStatement);
+	} catch (e) {
+		console.error(e);
+		res.send(`Something went wrong while running Puppeteer: ${e}`);
+	} finally {
+		await browser.close();
+	}
 };
 
 module.exports = { scrapeLogic };
