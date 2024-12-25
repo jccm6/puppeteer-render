90009be03f9d08595dc11a30973eac1224c6b23c
JC <jc@jccm.com.ve>
2024-12-25 13:25:59 -0300

    agregar argumentos

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const scrapeLogic = async (res) => {
 	const browser = await puppeteer.launch({
 		ignoreHTTPSErrors: true,
-		args: [`--proxy-server=${process.env.SCRAPERAPI_URI}, --disable-features=site-per-process`],
+		args: [`--proxy-server=${process.env.SCRAPERAPI_URI}, --disable-features=site-per-process`, "--disable-setuid-sandbox", "--no-sandbox", "--single-process", "--no-zygote"],
 		// executablePath: process.env.NODE_ENV === "production" ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
 	});
 
